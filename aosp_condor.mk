8d99e429cc5df4f0ff68cb43832356b4e56860d5
shubhmg <shubhmgajra2@gmail.com>
2016-02-18 00:11:22 +0530

    AOSP Bringup

--- aosp_condor.mk
+++ aosp_condor.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := aosp_condor
 PRODUCT_RELEASE_NAME := MOTO E
